fa9d1c13d1feafb8a3218e4da2d49ff5017e6143
Hueu123 <chuhueu1@gmail.com>
2021-12-12 22:03:38 +0700

    commit

---
+++ src/MODEL/departmentModel.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MODEL;
+
+/**
+ *
+ * @author chuhu
+ */
+public class departmentModel {
+    private String maphong;
+    private String tenphong;
+    private String leader;
+
+    public departmentModel() {
+    }
+
+    public departmentModel(String maphong, String tenphong, String leader) {
+        this.maphong = maphong;
+        this.tenphong = tenphong;
+        this.leader = leader;
+    }
+
+    public String getMaphong() {
+        return maphong;
+    }
+
+    public void setMaphong(String maphong) {
+        this.maphong = maphong;
+    }
+
+    public String getTenphong() {
+        return tenphong;
+    }
+
+    public void setTenphong(String tenphong) {
+        this.tenphong = tenphong;
+    }
+
+    public String getLeader() {
+        return leader;
+    }
+
+    public void setLeader(String leader) {
+        this.leader = leader;
+    }
+    
+}
---
+++ src/MODEL/employeeModel.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MODEL;
+
+/**
+ *
+ * @author chuhu
+ */
+public class employeeModel {
+
+    private String manv;
+    private String fullname;
+    private String birthday;
+    private String phoneNumber;
+    private String address;
+    private String position;
+    private String maphong;
+    private String mahoso;
+
+    public employeeModel() {
+    }
+
+    public employeeModel(String manv, String fullname, String birthday, String phoneNumber, String address, String position, String maphong, String mahoso) {
+        this.manv = manv;
+        this.fullname = fullname;
+        this.birthday = birthday;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.position = position;
+        this.maphong = maphong;
+        this.mahoso = mahoso;
+    }
+
+    public String getManv() {
+        return manv;
+    }
+
+    public void setManv(String manv) {
+        this.manv = manv;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getMaphong() {
+        return maphong;
+    }
+
+    public void setMaphong(String maphong) {
+        this.maphong = maphong;
+    }
+
+    public String getMahoso() {
+        return mahoso;
+    }
+
+    public void setMahoso(String mahoso) {
+        this.mahoso = mahoso;
+    }
+
+}
